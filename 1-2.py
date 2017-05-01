bbfe1487c0ca1ffc2a469df376720737a9f69168
Takayuki Kaisen <ksn0215@gmail.com>
2017-05-01 23:52:45 +0900

    added 1-2.py and 1-3.py

---
+++ 1-2.py
@@ -0,0 +1,14 @@
+import numpy as np
+import random
+
+T=1000000
+N=10
+
+result = []
+for t in range(T):
+  population = [i for i in range(1,N+1)]
+  samples = random.sample(population,N)
+  abs_val = abs(np.diff(samples)).sum()
+  result.append(abs_val)
+res = sum(result)/T
+print(res)
